52ea543671f9bbf991406a35794963b2900beedd
perla <plmolina2@miners.utep.edu>
2023-09-30 13:40:44 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -2,35 +2,38 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
-# Date Format: YYYY-MM-DD
-year = 2010
+#YYY-MM-DD
+year = '2010'
+country_code = 'ARG'
 
 def get_big_mac_price_by_year(year, country_code):
-   query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3.str.lower() == '{country_code.lower()}''))"
+   query_text = f"date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3.str.lower() == '{country_code.lower()}'"
    df_result = df.query(query_text)
    mean_dollar_price = df_result['dollar_price'].mean()
    return round(mean_dollar_price, 2)
    
 
 def get_big_mac_price_by_country(country_code):
-    query_text = f"iso_a3 == '{country_code.upper()}'"
+    query_text = f"iso_a3 == '{country_code}'"
     df_result = df.query(query_text)
     mean_dollar_price = df_result['dollar_price'].mean()
     return round(mean_dollar_price, 2)
 
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    query_text = f"date == '{year}'"
-    df_result = df.query(query_text)
-    cheapest_row = df_result.loc[df_result['dollar_price'].idxmin()]
-    return f"{cheapest_row['name']}({cheapest_row['iso_a3']}): ${round(cheapest_row['dollar_price'], 2):.2f}"
+    filtered_data = df[df['date'].astype(str) == str(year)]
+    cheapest_row = filtered_data.nsmallest(1, 'dollar_price').iloc[0]  
+    country_name = cheapest_row['name']
+    iso_a3 = cheapest_row['iso_a3']
+    dollar_price = cheapest_row['dollar_price']        
+    output = f"{country_name}({iso_a3}): ${round(dollar_price, 2)}"    
+    return output                          
     
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    query_text = f"date == '{year}'"
-    df_result = df.query(query_text)
-    expensive_row = df_result.loc[df_result['dollar_price'].idxmax()]
-    return f"{expensive_row['name']}({expensive_row['iso_a3']}): ${round(expensive_row['dollar_price'], 2):.2f}"
+    filtered_data = df[df['date'].astype(str) == str(year)]
+    expensive_row = filtered_data.nlargest(1, 'dollar_price').iloc[0]
+    return f"{expensive_row['name']}({expensive_row['iso_a3']}): ${round(expensive_row['dollar_price'], 2)}"
 
 
 if __name__ == "__main__":  
