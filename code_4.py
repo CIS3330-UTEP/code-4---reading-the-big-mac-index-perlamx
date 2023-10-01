6605a52269afe504f439672bee89a2145085e231
perla <plmolina2@miners.utep.edu>
2023-09-30 18:39:02 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -4,6 +4,8 @@ big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
 #YYY-MM-DD
 year = '2010'
+country_code = 'mys'
+dollar_price = '1.7'
 
 
 def get_big_mac_price_by_year(year, country_code):
@@ -25,24 +27,27 @@ def get_the_cheapest_big_mac_price_by_year(year):
     query_text = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     df_result = df.query(query_text)                    
     cheapest_row = df_result.loc[df_result['dollar_price'].idxmin()]
-    return f"{cheapest_row['name']}({cheapest_row['iso_a3']}): ${'%.1f' % cheapest_row['dollar_price']}"
+    return f"{cheapest_row['name']}({cheapest_row['iso_a3']}): ${'%.1f' %cheapest_row['dollar_price']}"
 
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     query_text = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     df_result = df.query(query_text)  
     expensive_row = df_result.loc[df_result['dollar_price'].idxmax()]
-    return f"{expensive_row['name']}({expensive_row['iso_a3']}): ${'%.1f' % expensive_row['dollar_price']}"
+    return f"{expensive_row['name']}({expensive_row['iso_a3']}): ${'%.1f' %expensive_row['dollar_price']}"
 
     
 
 if __name__ == "__main__":  
        result_a = get_big_mac_price_by_year(2010, "arg")
        print(result_a) 
+
        result_b = get_big_mac_price_by_country("mex")
        print(result_b) 
+
        result_c = get_the_cheapest_big_mac_price_by_year(2008)
        print(result_c) 
+       
        result_d = get_the_most_expensive_big_mac_price_by_year(2014)
        print(result_d) 
 
